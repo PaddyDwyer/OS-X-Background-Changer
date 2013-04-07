dc43c7207c4b84d31cdf461eb661bee7b13042df
Edgar Neto <edgar.figueiredo@gmail.com>
2013-04-07 03:14:46 +0200

    Added MacRuby to .plist file. Also, randomize an image from folder when there are less then 365 images.

--- cbg.rb
+++ cbg.rb
@@ -5,11 +5,20 @@ framework "Cocoa"
 
 yday = Time.new.yday
 path = File.expand_path(File.dirname(__FILE__))
+arr_images = Dir.glob("#{path}/images/")
+
+# tries to get image based on day of year
 image = Dir.glob("#{path}/images/#{yday}*")[0]
-puts yday
+
+# if fails, gets a random image from folder
+if (image == nil)
+	image = Dir.glob("#{path}/images/*.jpg").sample
+end
+
 wallpaper_url  = NSURL.fileURLWithPath(image, isDirectory: false)
 workspace      = NSWorkspace.sharedWorkspace
  
+# uses Macruby to change desktop. Works for the current space
 NSScreen.screens.each do |screen|
   workspace.setDesktopImageURL(wallpaper_url, forScreen:screen, options:nil, error:nil)
 end
