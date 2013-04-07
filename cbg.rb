6329868d791927460ea817c088f9cde6b37a8ac0
Edgar Neto <edgar.figueiredo@gmail.com>
2013-04-07 02:06:52 +0200

    Replaced rb-appscript with MacRuby

--- cbg.rb
+++ cbg.rb
@@ -1,11 +1,17 @@
-require "rubygems"
-require "appscript"
+#!/usr/bin/env macruby
 
-include Appscript, MacTypes
-
-path = File.expand_path(File.dirname(__FILE__))
+#Source: https://gist.github.com/jerodsanto/1229361
+framework "Cocoa"
 
 yday = Time.new.yday
+path = File.expand_path(File.dirname(__FILE__))
 image = Dir.glob("#{path}/images/#{yday}*")[0]
+puts yday
+wallpaper_url  = NSURL.fileURLWithPath(image, isDirectory: false)
+workspace      = NSWorkspace.sharedWorkspace
+ 
+NSScreen.screens.each do |screen|
+  workspace.setDesktopImageURL(wallpaper_url, forScreen:screen, options:nil, error:nil)
+end
 
-app('Finder').desktop_picture.set(FileURL.path(image))
+
